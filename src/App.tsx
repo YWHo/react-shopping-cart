9ac6adb3b9a82a2bae48da96e1d25066dc3ad3bf
Brian Ho <YWHo@users.noreply.github.com>
2024-01-22 23:46:38 +1300

    retrive products and display in vertical cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,34 @@ import { useQuery } from "react-query";
 // Components
 import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+import { CartItemType, getProducts } from "./apiRequests";
+import Item from "./Item/Item";
 import { Wrapper } from "./App.styles";
-export type CartItemType = {
-  id: number;
-  category: string;
-  description: string;
-  image: string;
-  price: number;
-  title: string;
-  amount: number;
-};
-
-async function getProducts(): Promise<CartItemType[]> {
-  const response = await fetch("https://fakestoreapi.com/products");
-  return await response.json();
-}
 
 function App() {
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
-  console.log(data);
-  return <div className="App">Start</div>;
+  console.log("App data:\n", data);
+  const getTotalItems = () => null;
+  const handleAddToCard = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCard} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 }
 
 export default App;
---
+++ src/apiRequests.ts
@@ -0,0 +1,27 @@
+import mockProduct from "./mockData/mockProduct";
+const HOST = process.env.REACT_APP_REMOTE_HOST;
+
+export type CartItemType = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+  image: string;
+  amount?: number;
+};
+
+export async function getProducts(): Promise<CartItemType[]> {
+  try {
+    const fullPath = `${HOST}/products`;
+    const response = await fetch(`${fullPath}`);
+    return await response.json();
+  } catch (err) {
+    // Use Mock data if the remote server is not working
+    return mockProduct();
+  }
+}
