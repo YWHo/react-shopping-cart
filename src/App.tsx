94433b7eac4ec13224137bd67463a1cc8c8b9eab
Brian Ho <YWHo@users.noreply.github.com>
2024-01-23 00:30:27 +1300

    show item total through shopping icon and badge

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,21 @@ import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
 import { CartItemType, getProducts } from "./apiRequests";
 import Item from "./Item/Item";
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
 
 function App() {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
   console.log("App data:\n", data);
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce(
+      (acc: number, item) => acc + (item.amount ? item.amount : 0),
+      0
+    );
   const handleAddToCard = (clickedItem: CartItemType) => null;
   const handleRemoveFromCart = () => null;
 
@@ -22,6 +28,18 @@ function App() {
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge
+          badgeContent={getTotalItems(cartItems)}
+          color="error"
+          overlap="rectangular"
+        >
+          <AddShoppingCart />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
